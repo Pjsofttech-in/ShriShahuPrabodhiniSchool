392cea70861e6a8de9e7b74d452755985d127090
Prapti Mangade <praptimangade@gmail.com>
2026-09-10 16:32:44 +0530

    ui chnages

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:8080").replace(/\/+$/g, "");
-const API_REQUEST_BASE_URL = API_BASE_URL.replace(/(?:\/api)+$/i, "");
+const API_REQUEST_BASE_URL = `${API_BASE_URL.replace(/\/+$/, "")}/api`;
 
 // Admin credentials for fetching live token
 const ADMIN_CREDENTIALS = {
